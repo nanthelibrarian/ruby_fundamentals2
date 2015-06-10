8dbc9524c7a97385396f86ffab473c0fe6e837bf
Nancy Wood <nancywood13@gmail.com>
2015-06-10 14:58:58 -0400

    made a list then added to it and counted the total number of items

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,20 @@
 grocery_list = ["steak", "potatoes", "salad", "eggs", "potato chips"]
- 
-grocery_list.each do |item|
+
+def print_grocery_list(list) 
+	list.each do |item|
 	puts "* #{item}"
+	end
 end
+
+print_grocery_list(grocery_list)
+
+grocery_list << "rice"
+
+print_grocery_list(grocery_list)
+
+puts grocery_list.count
+
+
+
+
+
