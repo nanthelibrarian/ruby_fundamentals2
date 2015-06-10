df228afdea7e8d6dd37238d7c7fe04e72d621884
Nancy Wood <nancywood13@gmail.com>
2015-06-10 16:41:22 -0400

    cohort names and numbers with expanded classroom numbers

---
+++ exercise7.rb
@@ -0,0 +1,29 @@
+students = {
+	:cohort1 => 34,
+	:cohort2 => 42,
+	:cohort3 => 22
+}
+
+def students_list(students)
+	students.each do | cohort, number |
+		puts "#{cohort}: #{number}"
+	end
+end 
+
+students[:cohort4] = 43
+
+students_list(students)
+
+def students_increase(students)
+	students.each do | cohort, number |
+		puts "#{cohort}: #{number.to_i * 1.05}"
+	end
+end
+
+students_increase(students)
+
+
+
+
+
+
