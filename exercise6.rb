963d1f89a7753dfd23fba513809448b7dfd884d2
Nancy Wood <nancywood13@gmail.com>
2015-06-10 16:01:41 -0400

    found the second item on our list then alphabetized list and also added context around each stop

--- exercise6.rb
+++ exercise6.rb
@@ -6,17 +6,30 @@ def print_grocery_list(list)
 	end
 end
 
+puts "Here is our grocery list so far"
 print_grocery_list(grocery_list)
 
 grocery_list << "rice"
 
+puts "oops!  forgot the rice.  Here is our revised grocery list"
 print_grocery_list(grocery_list)
 
+puts "how many items do we need to pick up today?"
 puts grocery_list.count
+
+puts "Wait.  What about bananas?  We need those for smoothies"
 
 if grocery_list.include?("bananas") == false 
 	puts "You need to pick up bananas"
 else puts "You don't need to pick up bananas today"
 
-
 end 
+
+puts "remind me: what was the second item on our list?"
+
+puts grocery_list[1]
+
+puts "hey, weird!  An alphabetically organized grocery store.  Let's sort our list to make this faster..."
+
+print_grocery_list(grocery_list.sort) 
+
